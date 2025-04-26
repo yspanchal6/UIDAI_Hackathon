b6464ec5b9800321b42f02bcf83612e75a4f31f4
yspanchal6 <yspanchal6@gmail.com>
2025-04-26 12:59:53 +0530

    Initial commit

---
+++ Python/Turtle/1.py
@@ -0,0 +1,114 @@
+import turtle as s
+s.bgcolor("grey10")
+s.pencolor('red')
+s.fillcolor("red")
+s.width(10)
+
+# I letter
+s.up()
+s.left(90)
+s.fd(100)
+s.left(90)
+s.fd(50)
+s.left(180)
+s.down()
+
+s.fd(100)
+s.left(180)
+s.up()
+s.fd(50)
+s.down()
+s.left(90)
+s.fd(100)
+
+
+s.right(90)
+s.fd(50)
+s.left(180)
+s.fd(100)
+
+# 
+
+
+# L Letter
+
+s.up()
+s.fd(80)
+s.left(90)
+s.fd(100)
+s.right(180)
+s.pendown()
+
+s.fd(100)
+s.left(90)
+s.fd(50)
+
+# ___
+s.up()
+s.fd(50)
+
+s.pendown()
+
+# o Letter
+s.circle(50)
+
+# # __
+s.up()
+s.fd(50)
+s.left(90)
+s.pendown()
+
+# V Letter
+s.up()
+s.fd(100)
+s.right(150)
+s.pendown()
+s.fd(100)
+s.left(120)
+s.fd(100)
+
+# ___
+s.up()
+s.right(180)
+s.fd(100)
+s.left(120)
+s.fd(70)
+s.down()
+# E
+s.up()
+s.left(90)
+s.fd(100)
+s.right(90)
+s.fd(50)
+s.left(180)
+s.down()
+
+s.fd(50)
+s.left(90)
+s.fd(50)
+s.left(90)
+s.fd(50)
+
+s.up()
+s.left(180)
+s.fd(50)
+s.left(90)
+s.down()
+
+s.fd(50)
+s.left(90)
+s.fd(50)
+
+# Y Letter
+
+# ____
+s.up()
+s.fd(80)
+s.left(90)
+s.fd(100)
+s.down()
+
+# U
+s.left(180)
+s.fd(95)
+
---
+++ Python/Turtle/2.py
@@ -0,0 +1,17 @@
+import turtle as s
+s.bgcolor("grey10")
+s.pencolor('red')
+s.fillcolor("red")
+s.width(10)
+
+
+s.begin_fill()
+
+s.left(50)
+s.fd(100)
+s.circle(38,200)
+s.right(140)
+s.circle(38,200)
+s.fd(100)
+s.end_fill()
+s.speed(10000)
